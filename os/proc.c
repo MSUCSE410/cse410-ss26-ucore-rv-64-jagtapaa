e41c6b506482ac0fe66309f42edab1ed49695f07
jagtapaa <jagtapaa@msu.edu>
2026-02-12 18:05:56 +0000

    hooped

--- os/proc.c
+++ os/proc.c
@@ -2,6 +2,7 @@
 #include "defs.h"
 #include "loader.h"
 #include "trap.h"
+#include "timer.h"
 
 struct proc pool[NPROC];
 char kstack[NPROC][PAGE_SIZE];
@@ -34,6 +35,9 @@ void proc_init(void)
 		/*
 		* LAB1: you may need to initialize your new fields of proc here
 		*/
+		p->start_cycle = 0;
+		memset(p->syscall_times, 0, sizeof(p->syscall_times));
+
 	}
 	idle.kstack = (uint64)boot_stack_top;
 	idle.pid = 0;
@@ -65,6 +69,8 @@ found:
 	memset(&p->context, 0, sizeof(p->context));
 	memset(p->trapframe, 0, PAGE_SIZE);
 	memset((void *)p->kstack, 0, PAGE_SIZE);
+	p->start_cycle = 0;
+	memset(p->syscall_times, 0, sizeof(p->syscall_times));
 	p->context.ra = (uint64)usertrapret;
 	p->context.sp = p->kstack + PAGE_SIZE;
 	return p;
@@ -84,6 +90,9 @@ void scheduler(void)
 				/*
 				* LAB1: you may need to init proc start time here
 				*/
+				if (p->start_cycle == 0) {
+					p->start_cycle = get_cycle();
+				}
 				p->state = RUNNING;
 				current_proc = p;
 				swtch(&idle.context, &p->context);
--- os/syscall.c
+++ os/syscall.c
@@ -39,6 +39,25 @@ uint64 sys_gettimeofday(TimeVal *val, int _tz)
 /*
 * LAB1: you may need to define sys_task_info here
 */
+uint64 sys_task_info(TaskInfo *ti)
+{
+	if (ti == 0)
+		return -1;
+
+	struct proc *p = curr_proc();
+
+	ti->status = Running;
+
+	for (int i = 0; i < MAX_SYSCALL_NUM; i++) {
+		ti->syscall_times[i] = p->syscall_times[i];
+	}
+
+	uint64 now = get_cycle();
+	ti->time = (now - p->start_cycle) * 1000 / CPU_FREQ;
+
+	return 0;
+}
+
 
 extern char trap_page[];
 
@@ -53,6 +72,10 @@ void syscall()
 	/*
 	* LAB1: you may need to update syscall counter for task info here
 	*/
+	if (id >= 0 && id < MAX_SYSCALL_NUM) {
+		curr_proc()->syscall_times[id]++;
+	}
+
 	switch (id) {
 	case SYS_write:
 		ret = sys_write(args[0], (char *)args[1], args[2]);
@@ -69,6 +92,17 @@ void syscall()
 	/*
 	* LAB1: you may need to add SYS_taskinfo case here
 	*/
+	#define SYS_task_info 410
+
+	#define SYS_pidfd_send_signal 424
+	#define SYS_io_uring_setup 425
+	#define SYS_io_uring_enter 426
+	#define SYS_io_uring_register 427
+
+	case SYS_task_info:
+	ret = sys_task_info((TaskInfo *)args[0]);
+	break;
+
 	default:
 		ret = -1;
 		errorf("unknown syscall %d", id);
